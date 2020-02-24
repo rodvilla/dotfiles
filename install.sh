742dd259e71c6daf123d4742a21e0316c215475e
Rodrigo Villalobos <rodrigovilla3@gmail.com>
2020-02-24 00:42:49 +0000

    Add automatic brew installation

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@
 # https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install from the backup brew file
+cd $DOTFILES_DIR && brew bundle
+
 ln -s "${DOTFILES_DIR}/gitconfig" ~/.gitconfig
 ln -s "${DOTFILES_DIR}/notable.json" ~/.notable.json
 ln -s "${DOTFILES_DIR}/tmux.conf" ~/.tmux.conf
 ln -s "${DOTFILES_DIR}/wakatime.cfg" ~/.wakatime.cfg
 ln -s "${DOTFILES_DIR}/zshrc" ~/.zshrc
 ln -s "${DOTFILES_DIR}/iterm" ~/.iterm
-ln -s "${DOTFILES_DIR}/Brewfile" ~/Brewfile
 ln -s "${DOTFILES_DIR}/ssh-config" ~/ssh/config
