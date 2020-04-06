450416350a58d8c2c3be156f9fd442ed9895ed72
Rodrigo Villalobos <rodrigovilla3@gmail.com>
2020-04-05 22:00:39 -0500

    Set exec permissions, update secrets scripts

--- secrets/backup.sh
+++ secrets/backup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Backup all of my secrets to Google Drive
-DESTINATION_FOLDER="~/Google\ Drive/Backups"
+DESTINATION_FOLDER="/Users/rodrigo/Google Drive/Backups/Secrets"
 
-cp ~/.wakatime.cfg $DESTINATION_FOLDER/wakatime.cfg
-cp ~/.config/karabiner/karabiner.json $DESTINATION_FOLDER/karabiner.json
-cp -r ~/.aws $DESTINATION_FOLDER/aws
-cp -r ~/.ssh $DESTINATION_FOLDER/ssh
+# Change dir to destination folder
+cd "$DESTINATION_FOLDER"
+
+cp ~/.wakatime.cfg ./wakatime.cfg
+cp ~/.config/karabiner/karabiner.json ./karabiner.json
+cp -r ~/.aws ./aws
+cp -r ~/.ssh ./ssh
