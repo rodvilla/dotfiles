828a88496f1b1742dabfa7f0126688556e332e04
Rodrigo Villalobos <rodrigovilla3@gmail.com>
2020-04-07 00:17:36 -0500

    Fix secrets script

--- secrets/install.sh
+++ secrets/install.sh
@@ -5,5 +5,5 @@ SECRETS_FOLDER="/Users/rodrigo/Google Drive/Backups/Secrets"
 
 ln -s "$SECRETS_FOLDER/wakatime.cfg" ~/.wakatime.cfg
 ln -s "$SECRETS_FOLDER/karabiner.json" ~/.config/karabiner/karabiner.json
-ln -s "$SECRETS_FOLDER/aws" ~./aws
-ln -s "$SECRETS_FOLDER/ssh"  ~./ssh
+ln -s "$SECRETS_FOLDER/aws" ~/.aws
+ln -s "$SECRETS_FOLDER/ssh"  ~/.ssh
