7bffc51f831ffca73d098d7e4835a4b5aea4de6c
Rodrigo Villalobos <rodrigovilla3@gmail.com>
2021-03-08 12:55:41 -0600

    Remove notable, update apps, clean zshrc

--- secrets/install.sh
+++ secrets/install.sh
@@ -3,6 +3,7 @@
 # Backup all of my secrets to Google Drive
 SECRETS_FOLDER="/Users/rodrigo/Google Drive/Backups/Secrets"
 
+ln -s "$SECRETS_FOLDER/.secrets"  ~/.secrets
 ln -s "$SECRETS_FOLDER/wakatime.cfg" ~/.wakatime.cfg
 ln -s "$SECRETS_FOLDER/karabiner.json" ~/.config/karabiner/karabiner.json
 ln -s "$SECRETS_FOLDER/aws" ~/.aws
